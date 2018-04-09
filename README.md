a97df18637f8604d7907d6726ff61ef3959f2ae7
JayQi <jayqizone@gmail.com>
2018-04-10 02:27:37 +0800

    Feature & optimize
    发送消息时若按住 ⌘ 键，可以发送并打开聊天窗口
    缓存搜索结果，减少网络 IO

--- README.md
+++ README.md
@@ -2,9 +2,11 @@
 
 ![platform](https://img.shields.io/badge/platform-macos-lightgrey.svg) ![language](https://img.shields.io/badge/language-javascript-yellow.svg) [![GitHub license](https://img.shields.io/github/license/jayqizone/WeChat-LaunchBar.svg)](https://github.com/jayqizone/WeChat-LaunchBar/blob/master/LICENSE)
 
-感谢 [TKkk-iOSer](https://github.com/TKkk-iOSer)，请先安装 [WeChatPlugin-MacOS](https://github.com/TKkk-iOSer/WeChatPlugin-MacOS) 插件
+感谢 [TKkk-iOSer](https://github.com/TKkk-iOSer)，请先安装 [WeChatPlugin-MacOS](https://github.com/TKkk-iOSer/WeChatPlugin-MacOS) 微信小助手插件
 
-## 使用
+## 用法
+
+- 克隆或下载本项目，双击安装 WeChat Plugin.lbaction
 
 - 调出 LaunchBar，输入 WX，按空格或回车激活 Action
 
@@ -16,6 +18,10 @@
 
 ![](https://raw.githubusercontent.com/jayqizone/WeChat-LaunchBar/master/images/open.png)
 
-- 如果想直接发送消息，用分号（半角全角皆可）分隔，输入文字回车即可
+- 如果想后台直接发送消息，用分号（半角全角皆可）分隔，输入文字回车即可（若按住 ⌘ 键则可以发送并打开聊天窗口）
 
 ![](https://raw.githubusercontent.com/jayqizone/WeChat-LaunchBar/master/images/send.png)
+
+## 已知问题
+
+- 搜索好友或群时若输入字符过快，可能导致微信 CPU 占用居高不下，疑似微信小肋手高并发查询时存在性能问题
--- WeChat Plugin.lbaction/Contents/Scripts/default.js
+++ WeChat Plugin.lbaction/Contents/Scripts/default.js
@@ -1,20 +1,21 @@
 // LaunchBar Action Script
 let baseUrl = 'http://127.0.0.1:52700/wechat-plugin/';
+let tmp = '~/.wechat-plugin.tmp';
+let timeout = 1;
 
 function run(argument) {
-    let url = baseUrl + 'user?keyword=';
-
     let index_en = argument.indexOf(';');
     let index_cn = argument.indexOf('；');
     let index;
+
     if (index_en < 0 && index_cn < 0) {
         index = argument.length;
     }
     else if (index_en < 0) {
-        index = index_cn
+        index = index_cn;
     }
     else if (index_cn < 0) {
-        index = index_en
+        index = index_en;
     }
     else {
         index = index_en < index_cn ? index_en : index_cn;
@@ -22,36 +23,54 @@ function run(argument) {
 
     let keyword = argument.substr(0, index);
     let content = argument.substr(index + 1);
-
-    let response = HTTP.getJSON(url + encodeURIComponent(keyword));
     let result = [];
 
-    if (response == undefined) {
-        LaunchBar.alert('HTTP.getJSON() returned undefined');
-        return [];
-    }
+    if (keyword) {
+        let last, response;
 
-    if (response.error != undefined) {
-        LaunchBar.log('Error in HTTP request: ' + response.error);
-        return [];
-    }
+        File.exists(tmp) && (last = File.readJSON(tmp));
+        if (last) {
+            if (keyword == last.keyword || (keyword.startsWith(last.keyword) && !last.result.length)) {
+                result = last.result;
+                result.forEach(e => e.actionArgument.content = content);
+            } else {
+                response = true;
+            }
+        } else {
+            response = true;
+        }
 
-    response.data.forEach(e => {
-        result.push({ title: e.title, subtitle: e.subTitle, icon: e.icon, action: 'action', actionArgument: { userId: e.userId, content: content } })
-    });
+        if (response) {
+            try {
+                response = HTTP.getJSON(baseUrl + 'user?keyword=' + encodeURIComponent(keyword), { timeout: timeout });
+                if (response) {
+                    if (response.error) {
+                        LaunchBar.alert(response.error);
+                    } else {
+                        response.data.forEach(e => result.push({ title: e.title, subtitle: e.subTitle, icon: e.icon, action: 'action', actionArgument: { userId: e.userId, content: content } }));
+
+                        File.writeJSON({ keyword: keyword, result: result }, tmp);
+                    }
+                }
+            }
+            catch (e) {
+                LaunchBar.alert(e);
+            }
+        }
+    }
 
     return result;
 }
 
 function action(data) {
-    let url = baseUrl;
-    if (data.content) {
-        url += 'send-message';
-    } else {
-        url += 'open-session';
+    if (!data.content || LaunchBar.options.commandKey) {
+        LaunchBar.execute('/usr/bin/open', '/Applications/WeChat.app');
+        HTTP.post(baseUrl + 'open-session', { body: data, timeout: timeout });
     }
 
-    HTTP.post(url, { body: data });
+    if (data.content) {
+        HTTP.post(baseUrl + 'send-message', { body: data, timeout: timeout });
+    }
 
     LaunchBar.hide();
 }
